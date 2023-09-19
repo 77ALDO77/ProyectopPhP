44f30b2fcc8e45e8054a8bde54df1dd82e7b98db
Raul_LA <rleonalvarez090@gmail.com>
2023-09-19 05:25:09 -0500

    Encabezado en Inicio, problemas al colocar el footer

--- Inicio.php
+++ Inicio.php
@@ -22,33 +22,39 @@
     </style>
 </head>
 <body>
-    <h1>Bienvenido a la Tienda de Videojuegos</h1>
+    <?php include('EncabezadoInicio.php'); ?>
+    
+    <main>
+    
+        <h1>Bienvenido a la Tienda de Videojuegos</h1>
 
-    <h2>Nuestros Productos Destacados:</h2>
+        <h2>Nuestros Productos Destacados:</h2>
 
-    <?php
-        // Array de productos con nombre, descripción y precio
-        $productos = array(
-            array("nombre" => "FIFA 22", "descripcion" => "Simulador de fútbol", "precio" => 60),
-            array("nombre" => "Cyberpunk 2077", "descripcion" => "RPG de ciencia ficción", "precio" => 70),
-            array("nombre" => "The Last of Us Part II", "descripcion" => "Aventura y acción", "precio" => 50)
-        );
+        <?php
+            // Array de productos con nombre, descripción y precio
+            $productos = array(
+                array("nombre" => "FIFA 22", "descripcion" => "Simulador de fútbol", "precio" => 60),
+                array("nombre" => "Cyberpunk 2077", "descripcion" => "RPG de ciencia ficción", "precio" => 70),
+                array("nombre" => "The Last of Us Part II", "descripcion" => "Aventura y acción", "precio" => 50)
+            );
 
-        // Función para mostrar productos
-        function mostrarProductos($productos) {
-            foreach ($productos as $producto) {
-                echo "<div class='producto'>";
-                echo "<p class='nombre'>{$producto['nombre']}</p>";
-                echo "<p>{$producto['descripcion']}</p>";
-                echo "<p>Precio: {$producto['precio']} USD</p>";
-                echo "<button>Agregar al Carrito</button>";
-                echo "</div>";
+            // Función para mostrar productos
+            function mostrarProductos($productos) {
+                foreach ($productos as $producto) {
+                    echo "<div class='producto'>";
+                    echo "<p class='nombre'>{$producto['nombre']}</p>";
+                    echo "<p>{$producto['descripcion']}</p>";
+                    echo "<p>Precio: {$producto['precio']} USD</p>";
+                    echo "<button>Agregar al Carrito</button>";
+                    echo "</div>";
+                }
             }
-        }
 
-        // Llamar a la función para mostrar productos
-        mostrarProductos($productos);
-    ?>
+            // Llamar a la función para mostrar productos
+            mostrarProductos($productos);
+        ?>  
+    </main>
+    
 </body>
 </html>
 
