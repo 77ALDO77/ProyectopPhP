dfb38c14fdaeda308b8ff791e61b96da395848ca
Prueba <Prueba@DESKTOP-FIMVOIG>
2023-09-18 22:48:12 -0500

    Agrego Contactenos_Ubiquenos

---
+++ mainmapa.php
@@ -0,0 +1,6 @@
+<main>
+            <div id="mapa">
+            <aside>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15610.147998596518!2d-77.059821!3d-12.0065059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105ce300d8854e1%3A0x6a1b58f36fcc79a6!2sPlaza%20Norte!5e0!3m2!1ses-419!2spe!4v1695091535602!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>            </aside>
+                </div>
+        </main>
