19b4a4518ed9082002a1f7c16e895e5b03857acd
Raul_LA <rleonalvarez090@gmail.com>
2023-09-19 15:05:32 -0500

    Pagina de Inicio

--- index.php
+++ index.php
@@ -1,3 +1,119 @@
-<?php include('EncabezadoInicio.php'); ?>
-<main><!-- Escribir contenido de inicio (Aldo) --></main>
-<?php include('PieInicio.php'); ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="CSS/PieInicio.css" rel="stylesheet" type="text/css"/>
+    <title>TECHNOGAME</title>
+    <link href="CSS/Inici.css" rel="stylesheet" type="text/css"/>
+</head>
+
+    
+    <?php include('EncabezadoInicio.php'); ?>
+  
+    
+    <body>
+        
+     <main class="contenedor">
+                <h1 class="nustros-productos">Nustros Productos</h1>
+                <!--INICIO PRODUCTOS-->
+                <div class="productos">
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/funko-pop-disney-classics-mickey-mouse_1.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Funko pop Mickey</p>
+                            <p class="producto-precio">$205</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/Monopoly.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Monopoly</p>
+                            <p class="producto-precio">$45</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/super_smash_bros_ultimate_switch_1.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Super Smash Bros</p>
+                            <p class="producto-precio">$249</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/silla_Razer.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Silla Gamer Razer</p>
+                            <p class="producto-precio">$1599</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/ps5_consola_spiderman_2_2_.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">PS5 Edc. Spaiderman</p>
+                            <p class="producto-precio">$3999</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">                                           
+                            <img src="Imagenes/pokemoncard.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Pokémon Trading Card Game</p>
+                            <p class="producto-precio">$139</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/marvel_legends_hulk_y_bruce_banner_avengers_1_.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Marvel Legends Series - Hulk gris y Dr. Bruce Banner</p>
+                            <p class="producto-precio">$360</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/marvel_legends_gamerverse_-_spider-man_2_0_.jpg" alt=""/>
+                        </div>
+
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Marvel Legends Gamerverse - Spider-Man 2</p>
+                            <p class="producto-precio">$200</p>
+                        </div>
+                    </div>
+                    <!---INICIO PRODUCTO-->
+                    <div class="producto">
+                        <div class="producto-blanco">
+                            <img src="Imagenes/mando_xbox_series_x_negro_1_.jpg" alt=""/>
+                        </div>
+                        <div class="producto-informacion">
+                            <p class="producto-nombre">Mando Xbox Inalámbrico para Xbox Series X/S, One, PC - Negro Carbón</p>
+                            <p class="producto-precio">$330</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <!--FIN DEL CONTENIDO PRINCIPAL-->
+
+    </body>
+    <?php include('PieInicio.php'); ?>
+    
+</html>
