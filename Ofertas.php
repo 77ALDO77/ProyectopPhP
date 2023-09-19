eef846c3700067bb1f62e3fa1972c94d65c9a608
juand <juand@JUAN_DIEGO>
2023-09-18 20:12:47 -0500

    Ofertas

---
+++ Ofertas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ofertas</title>
+    </head>
+    <body>
+        <header>
+            <div id="Technogame"><b>TECHNOGAME</b></div>
+        </header>
+        <div id="Panel1">
+            <center>
+            <img src="IMG/SM2.jpg" alt="Preventa Spider-Man 2"/>
+            </center>
+        </div>
+        <br>
+        <div id="Panel2">
+            <center>
+            <img src="IMG/FC24.jpg" alt="Preventa FC24"/>
+            </center>
+        </div>
+        <br>
+        <div id="Panel3">
+            <center>
+            <img src="IMG/MK1.jpg" alt="Preventa Mortal Kombat 1"/>
+            </center>
+        </div>
+    </body>
+</html>
