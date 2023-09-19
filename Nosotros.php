7d3e52dbea35743804ae4227bd5d7695ec4abbca
juand <juand@JUAN_DIEGO>
2023-09-18 19:16:45 -0500

    Nosotros

---
+++ Nosotros.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Nosotros</title>
+    </head>
+    <body>
+        <header>
+            <div id="Technogame"><b>TECHNOGAME</b></div> 
+        </header>
+        <div id="Quienessomos">
+            <h2><b>¿Quiénes somos?</b></h2>
+            Somos una empresa dedicada a la comercialización de productos de entretenimiento,
+            con tiendas físicas ubicadas en varios distritos de la ciudad de Lima
+            y con cobertura mediante nuestra página web.
+        </div>
+        <br>
+        <div id="Vision">
+            <h2><b>VISIÓN</b></h2>
+            Ser una plataforma retail de la industria del entretenimiento electrónico, 
+            coleccionables y afines a la cultura pop en el Perú, con el propósito de 
+            generar valor en la experiencia de compra de nuestros clientes.
+        </div>
+        <br>
+        <div id="Mision">
+            <h2><b>MISIÓN</b></h2>
+            Lograr ser la marca líder en la industria del entretenimiento electrónico, 
+            coleccionables y afines a la cultura pop, brindando un servicio excelente 
+            a nuestros clientes.
+        </div>
+    </body>
+</html>
