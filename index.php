be90caf216d19aac4e7ebb21311f7614bfd23777
Jhayr <jhayrcerna10@gmail.com>
2023-09-18 20:10:11 -0500

    Folder de imagenes

--- index.php
+++ index.php
@@ -1,12 +1,101 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="CSS/EncabezadoInicio.css" rel="stylesheet" type="text/css"/>
+    <link href="CSS/PieInicio.css" rel="stylesheet" type="text/css"/>
+    <title>TECHNOGAME</title>
+    
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        Hola :DDd
-        ?>
-    </body>
+</head>
+<body>
+       <header class="Encabezado">
+           <a href="#" class="Logo">
+                <img src="Imagenes/LogoTechnoGame.jpeg" alt="Logo de la compañia"/>
+                <h2 class="NombreTienda">TECHNOGAME</h2></a>
+           
+           <nav  class="OpcionesEncabezado">
+                   <a href="#" class="Inicio">Inicio</a>
+                   <a href="#" class="Comunidad">Comunidad</a>
+                   <a href="#" class="Reservar">Reservar</a>
+                   <a href="#" class="Ofertas">Ofertas</a>
+                   <a href="#" class="Contacto">Contacto</a>
+                   <a href="#" class="Servicio al cliente">Servicio al cliente</a>
+                   <a href="#" class="Comunidad">Sobre nosotros</a>
+               
+           </nav>
+        </header>
+
+    <main> 
+<!-- CONTENIDO DE LA PÁGINA !!!!!!!!!!!!!-->
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    </main> 
+    
+    
+    
+    
+    
+    <footer >
+        <div class="PieDePagina">
+            <div class="contacto">
+                <h3><a href="#">CONTÁCTENOS</a></h3>
+                <hr></hr>
+                <h4><strong><h4>Redes Sociales</h4></strong></h4>
+                <hr></hr> 
+                <h4><p><strong>Teléfono</strong></p></h4>
+                <ul>
+                    <li>(+51) 964593567</li>
+                </ul>
+                <hr> </hr>
+                <h4><p><strong>Horarios</strong></p></h4>
+                <ul><li>Lun-Sab: 8:00 am - 22:00 pm</li></ul>
+                <hr></hr> 
+                <h4><p><strong>Email</strong></p></h4>
+                <ul>  
+                    <li>TechnoGame@gmail.com</li
+                    </ul>
+               
+            </div>
+
+            <div class="enlaces-importantes">
+                <h3>ENLACES IMPORTANTES</h3>
+                <hr> </hr>
+                <h4><a href="#">Libro de reclamaciones</a></h4>
+                <hr></hr>
+                <h4><a href="#">Sugerencias</a></h4>
+            </div> 
+
+            <div class="Informacion">
+                <h3><strong>INFORMACIÓN</strong></h3>
+                <hr></hr>
+                <h4><strong><a href="#">Nosotros</a></strong></h4>
+                 <hr></hr>  
+                <h4><strong><a href="#">Nuestras tiendas</a></strong></h4>
+                <ul>
+                <li>Plaza Norte</li>
+                <li>Mega Plaza</li>
+                <li>Real Plaza</li>
+                </ul>
+                <hr></hr>
+                <h4><strong><a href="#">¿Cómo comprar?</a></strong></h4>
+                <hr></hr>
+                <h4><strong><a href="#">Preguntas frecuentes</a></strong></h4>
+            </div>
+        </div>
+        
+    </footer>
+</body>
 </html>
