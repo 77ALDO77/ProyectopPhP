e8922a7e9670c33820166779823622f29637a81d
Raul_LA <rleonalvarez090@gmail.com>
2023-09-19 00:35:58 -0500

    mod frm Sugerencias

--- FrmSugerencias.php
+++ FrmSugerencias.php
@@ -11,35 +11,38 @@
     <form class="form" action="ProcesaForms.php" method="POST">
         <h1 class="form_title">Formulario de Sugerencias</h1>
         <p class="form_paragraph">Por favor, déjanos tus sugerencias:</p>
-        <div class="form_container">
-          
-            <input type="text" id="nombre" class="input" required="">   
-            <label for="nombre" class="label">Nombres y Apellidos:</label>
-            <span class="form_line"></span>
+        <div class="form_group">
+            
+            <div class="form_container">
+
+                <input type="text" id="nombre" class="input" required="">   
+                <label for="nombre" class="label">Nombres y Apellidos:</label>
+                <span class="form_line"></span>
+            </div>
+
+            <div class="form_container">
+
+                <input type="email" id="email" class="input" required="">   
+                <label for="email" class="label">Correo:</label>
+                <span class="form_line"></span>
+            </div>
+
+            <div class="form_container">
+
+                <input type="text" id="numero" class="input" required="">   
+                <label for="numero" class="label">Celular:</label>
+                <span class="form_line"></span>
+            </div>
+
+            <div class="form_container">
+
+                <textarea id="sugerencia" rows="4" cols="50" required=""></textarea>  
+                <label for="sugerencia" class="label">Detalle de la Sugerencia:</label>
+                <span class="form_line"></span>
+            </div>
+
+            <button class="btn">Enviar</button>
         </div>
-        
-        <div class="form_container">
-          
-            <input type="email" id="email" class="input" required="">   
-            <label for="email" class="label">Correo:</label>
-            <span class="form_line"></span>
-        </div>
-        
-        <div class="form_container">
-          
-            <input type="text" id="numero" class="input" required="">   
-            <label for="numero" class="label">Celular:</label>
-            <span class="form_line"></span>
-        </div>
-        
-        <div class="form_container">
-          
-            <textarea id="sugerencia" rows="4" cols="50" required=""></textarea>  
-            <label for="sugerencia" class="label">Detalle de la Sugerencia:</label>
-            <span class="form_line"></span>
-        </div>
-        
-        <button class="btn">Enviar</button>
     </form>
 </body>
 </html>
