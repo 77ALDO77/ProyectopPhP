a53eea51b9600f346f39e2b59427f8641ce30223
Prueba <Prueba@DESKTOP-FIMVOIG>
2023-10-08 18:22:10 -0500

    Subo Inicio Actualizado

--- Inicio.php
+++ Inicio.php
@@ -1,110 +1,197 @@
-<!DOCTYPE html>
-<html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>Tienda de Videojuegos</title>
-    <link href="CSS/Inici.css" rel="stylesheet" type="text/css"/>
+    <link href="css3/StyleInicio.css" rel="stylesheet" type="text/css"/>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 </head>
 <body>
-     <main class="contenedor">
-                <h1 class="nustros-productos">Nuestros Productos</h1>
-                <!--INICIO PRODUCTOS-->
-                <div class="productos">
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/funko-pop-disney-classics-mickey-mouse_1.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Funko pop Mickey</p>
-                            <p class="producto-precio">$205</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/Monopoly.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Monopoly</p>
-                            <p class="producto-precio">$45</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/super_smash_bros_ultimate_switch_1.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Super Smash Bros</p>
-                            <p class="producto-precio">$249</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/silla_Razer.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Silla Gamer Razer</p>
-                            <p class="producto-precio">$1599</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/ps5_consola_spiderman_2_2_.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">PS5 Edc. Spaiderman</p>
-                            <p class="producto-precio">$3999</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">                                           
-                            <img src="Imagenes/pokemoncard.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Pokémon Trading Card Game</p>
-                            <p class="producto-precio">$139</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/marvel_legends_hulk_y_bruce_banner_avengers_1_.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Marvel Legends Series - Hulk gris y Dr. Bruce Banner</p>
-                            <p class="producto-precio">$360</p>
-                        </div>
-                    </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/marvel_legends_gamerverse_-_spider-man_2_0_.jpg" alt=""/>
-                        </div>
 
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Marvel Legends Gamerverse - Spider-Man 2</p>
-                            <p class="producto-precio">$200</p>
-                        </div>
+    <div class="contenedorproductos">
+        <!--<h1 class="titulo">Categorías</h1>
+        <!--INICIO PRODUCTOS-->
+        <div class="productos">
+            <!---INICIO PRODUCTO-->
+            <div class="producto">
+                <div class="producto-blanco">
+                    <img src="ImagenesInicio/imagen1.jpg" alt=""/>
+                </div>
+                <div class="producto-informacion">
+                    <p class="producto-nombre">Consolas</p>
+                </div>
+            </div>
+            <!---INICIO PRODUCTO-->
+            <div class="producto">
+                <div class="producto-blanco">
+                    <img src="ImagenesInicio/imagen2.jpg" alt=""/>
+                </div>
+                <div class="producto-informacion">
+                    <p class="producto-nombre">Juegos</p>
+                </div>
+            </div>
+            <!---INICIO PRODUCTO-->
+            <div class="producto">
+                <div class="producto-blanco">
+                    <img src="ImagenesInicio/imagen3.jpg" alt=""/>
+                </div>
+                <div class="producto-informacion">
+                    <p class="producto-nombre">Perifericos</p>
+                </div>
+            </div>
+            <!---INICIO PRODUCTO-->
+            <div class="producto">
+                <div class="producto-blanco">
+                    <img src="ImagenesInicio/imagen4.jpg" alt=""/>
+                </div>
+                <div class="producto-informacion">
+                    <p class="producto-nombre">Sillas Gamer</p>
+                </div>
+            </div>
+            <!---INICIO PRODUCTO-->
+            <div class="producto">
+                <div class="producto-blanco">
+                    <img src="ImagenesInicio/imagen5.jpg" alt=""/>
+                </div>
+                <div class="producto-informacion">
+                    <p class="producto-nombre-mando">Mandos<br>Personalizados</p>
+                </div>
+            </div>
+            <!---INICIO PRODUCTO-->
+            <div class="producto">
+                <div class="producto-blanco">                                           
+                    <img src="ImagenesInicio/imagen6.jpg" alt=""/>
+                </div>
+                <div class="producto-informacion">
+                    <p class="producto-nombre">Coleccionables</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Pre-venta -->
+    <div class="contenedorpreventa">
+        <h2 class="titulopreventa">Preventa</h2>
+        <div class="productospreventa">
+            <div class="productopreve">              
+                    <div class="productopre-imagen">
+                    <img src="ImagenesInicio/imagenpre1.jpg" alt=""/>
                     </div>
-                    <!---INICIO PRODUCTO-->
-                    <div class="producto">
-                        <div class="producto-blanco">
-                            <img src="Imagenes/mando_xbox_series_x_negro_1_.jpg" alt=""/>
-                        </div>
-                        <div class="producto-informacion">
-                            <p class="producto-nombre">Mando Xbox Inalámbrico para Xbox Series X/S, One, PC - Negro Carbón</p>
-                            <p class="producto-precio">$330</p>
-                        </div>
+                <div class="productopreventa-informacion">
+                    <p class="productopreventa-disponabilidad">Disponible</p>
+                    <p class="productopreventa-nombre">Call of Duty: Modern Warfare III (PS5) – Preventa</p>
+                    <p class="productopreventa-precio">S/249.99</p>
+                </div>
+            </div>
+            <div class="productopreve">              
+                    <div class="productopre-imagen">
+                    <img src="ImagenesInicio/imagenpre2.jpg" alt=""/>
+                    </div>
+                <div class="productopreventa-informacion">
+                    <p class="productopreventa-disponabilidad">Disponible</p>
+                    <p class="productopreventa-nombre">Avatar: Frontiers of Pandora (PS5) - Preventa</p>
+                    <p class="productopreventa-precio">S/199.99</p>
+                </div>
+            </div>
+            <div class="productopreve">              
+                    <div class="productopre-imagen">
+                        <img src="ImagenesInicio/imagenpre3.jpg" alt=""/>
+                    </div>
+                <div class="productopreventa-informacion">
+                    <p class="productopreventa-disponabilidad">Agotado</p>
+                    <p class="productopreventa-nombre">Metal Gear Solid: Master Collection Vol. 1 (PS5) - Preventa</p>
+                    <p class="productopreventa-precio">S/279.99</p>
+                </div>
+            </div>
+            <div class="productopreve">              
+                    <div class="productopre-imagen">
+                    <img src="ImagenesInicio/imagenpre4.jpg" alt=""/>
+                    </div>
+                <div class="productopreventa-informacion">
+                    <p class="productopreventa-disponabilidad">Disponible</p>
+                    <p class="productopreventa-nombre">Just Dance 2024 (PS5) - Preventa</p>
+                    <p class="productopreventa-precio">S/209.99</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Ofertas -->
+    <div class="contenedorofertas">
+        <h2 class="tituloofertas">Ofertas</h2>
+        <div class="productosoferta">
+            <div class="productofe">              
+                    <div class="productofe-imagen">
+                    <img src="ImagenesInicio/imagenofe1.jpg" alt=""/>
+                    </div>
+                <div class="productofe-informacion">
+                    <p class="productofe-disponabilidad">Disponible</p>
+                    <p class="productofe-nombre">Razer Huntsman Mini Mecánico Chroma Teclado - Blanco</p>
+                    <div class="productofe-precio">
+                    <p class="productofee-precio">S/634.99</p>
+                    <p>S/499.90</p>
                     </div>
                 </div>
-            </main>
-            <!--FIN DEL CONTENIDO PRINCIPAL-->
+            </div>
+            <div class="productofe">              
+                    <div class="productofe-imagen">
+                    <img src="ImagenesInicio/imagenofe2.jpg" alt=""/>
+                    </div>
+                <div class="productofe-informacion">
+                    <p class="productofe-disponabilidad">Disponible</p>
+                    <p class="productofe-nombre">Sony Inzone H3 Audífonos Multiplataforma - Blanco</p>
+                    <div class="productofe-precio">
+                    <p class="productofee-precio">S/659.99</p>
+                    <p>S/249.99</p>
+                    </div>
+                </div>
+            </div>
+            <div class="productofe">              
+                    <div class="productofe-imagen">
+                        <img src="ImagenesInicio/imagenofe3.jpg" alt=""/>
+                    </div>
+                <div class="productofe-informacion">
+                    <p class="productofe-disponabilidad">Agotado</p>
+                    <p class="productofe-nombre">Razer Hammerhead True Wireless X IPX4 Bluetooth USB-C Black - Audífonos Inalámbricos</p>
+                    <div class="productofe-precio">
+                    <p class="productofee-precio">S/451.99</p>
+                    <p>S/259.99</p>
+                    </div>
+                </div>
+            </div>
+            <div class="productofe">              
+                    <div class="productofe-imagen">
+                    <img src="ImagenesInicio/imagenofe4.jpg" alt=""/>
+                    </div>
+                <div class="productofe-informacion">
+                    <p class="productofe-disponabilidad">Disponible</p>
+                    <p class="productofe-nombre">Silla Gamer Corsair TC200 Tela Negro/Negro</p>
+                    <div class="productofe-precio">
+                    <p class="productofee-precio">S/1599.99</p>
+                    <p>S/1329.99</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-    </body>
+    <!--
+<script type="text/javascript">
+        let numpro = 0;
+        for (var i = 1; i <= 6; i++) {
+            numpro = (++numpro % 6 == 0) ? 1 : numpro;
+            movida = "flip-left";
+            if (numpro % 2 == 0) {
+                movida = "flip-up";
+            } else if (numpro % 3 == 0) {
+                movida = "zoom-in-up";
+            }
+            let prod = `
+    <div class="producto" data-aos="${movida}" data-aos-duration="1500">
+        <img src="Imagenespr/imagen${numpro}.jpg" alt=""/>
+</div>`;
+            document.write(prod);
+        }
+    </script>
+    <script type="text/javascript">
+        AOS.init();
+    </script>-->
+</body>
 </html>
-
