e93aef7438282f63a9d5caf5491746629aadebe4
Raul_LA <rleonalvarez090@gmail.com>
2023-10-08 00:38:46 -0500

    mods frm Ubicanos/Contactanos

--- contactenos_ubiquenos.php
+++ contactenos_ubiquenos.php
@@ -1,113 +1,248 @@
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>TecnoGamer</title>
+        <title>sw ABC</title>
         <link href="CSS/EstiloContactenos_Ubiquenos.css" rel="stylesheet" type="text/css"/>
-        <?php include('EncabezadoInicio.php'); ?>
     </head>
-    
+        
+    <?php include('EncabezadoInicio.php'); ?>
     <body>
         <div id="contenedor-cont_ubic">
-            <header>
-                <div id="contenedorredes">
-                    <div id="contacto"><center><h1>Contáctenos</h1></center></div>
-                    <div id="redesSociales"><h3>Redes sociales</h3></div>
-                    <div id="inforedes"><h3>¿Tienes alguna pregunta o inquietud? ¡Conéctate con nosotros en nuestras redes sociales y obtén respuestas rápidas y personalizadas! 
+ 
+            <div id="contacto"><center><h1>Contáctenos</h1></center></div>
+            <div class="container">
+                
+                <div class="redes">
+                    <h2 class="title">Redes sociales</h2>
+                    <p class="info_redes">¿Tienes alguna pregunta o inquietud? ¡Conéctate con nosotros en nuestras redes sociales y obtén respuestas rápidas y personalizadas! 
                             Nuestro equipo está listo para atenderte y proporcionarte la información que necesitas. Únete a nuestra comunidad en las redes sociales y descubre una
-                            nueva forma de interactuar con nosotros. ¡Esperamos conocerte pronto!</h3></div>
-                    <div id="imagredes1">
-                        <a href="https://www.facebook.com" target="_blank">
-                            <img src="Imagenes/Logo_de_Facebook.png" alt="Facebook"/>
-                        </a>
-                        <a href="https://www.youtube.com" target="_blank">
-                            <img src="Imagenes/LogoYoutube.png" alt="Youtube"/>
-                        </a>
+                            nueva forma de interactuar con nosotros. ¡Esperamos conocerte pronto!</p>
+                    
+                    <div class="container_imgs">
+
+                        <div class="itemImg"><a href="https://www.facebook.com"  class="enlace-imagen">
+                                <img src="Imagenes/Logo_de_Facebook.png" alt="Facebook"/>
+                            </a></div>
+
+                            <div class="itemImg"><a href="https://www.youtube.com" class="enlace-imagen">
+                                <img src="Imagenes/LogoYoutube.png" alt="Youtube"/>
+                            </a></div>
+                            
+                            <div class="itemImg"><a href="https://www.instagram.com" class="enlace-imagen">
+                                <img src="Imagenes/LogoInstagram.jpg" alt="Instagram"/>
+                            </a></div>
+              
+                        
+                           <div class="itemImg"><a href="https://www.twitter.com" class="enlace-imagen">
+                                <img src="Imagenes/LogoTwitter.jpg" alt="Twitter"/>
+                            </a></div>
+                    </div>           
+                </div> <!-- Contenedor para apartado redes-->
+                
+                <div class="container_contacto">
+                    
+                    <div class="item">
+                       <h2 class="title">Teléfono</h2>
+                       <p class="info">¿Necesitas ayuda? Llámanos. Nuestro equipo estará encantado de atenderte en horario laboral. 
+                               Si nos contactas fuera de ese horario, nuestro bot te proporcionará información básica hasta que estemos disponibles para asistirte.</p>
+                       <div class="cont_number">
+                           <div><img src="Imagenes/LogoTelefono.png" alt=""/></div>
+                           <div class="number"><h4>(+51)964593567</h4></div>
+                       </div>
                     </div>
-                    <div id="imagredes2">
-                        <a href="https://www.instagram.com" target="_blank">
-                            <img src="Imagenes/LogoInstagram.jpg" alt="Instagram"/>
-                        </a>
-                        <a href="https://www.twitter.com" target="_blank">
-                            <img src="Imagenes/LogoTwitter.jpg" alt="Twitter"/>
-                        </a>
+
+                    <div class="item">
+                        <h2 class="title">Horario</h2>
+                        <p class="info">¡Estamos a tu disposición! Nuestro equipo está disponible para atenderte en el siguiente horario:</p>
+                        <p class="info_h">Lunes a Sábado: 8:00 AM - 10:00 PM<p>
                     </div>
-                </div>
-                <div id="telefono"><h3>Teléfono</h3>
-                </div>
-                <div id="infotelefono"><h4>(+51)964593567</h4>
-                </div>
-                <div id="infotelefono2"><h4>¿Necesitas ayuda? Llámanos. Nuestro equipo estará encantado de atenderte en horario laboral. 
-                        Si nos contactas fuera de ese horario, nuestro bot te proporcionará información básica hasta que estemos disponibles para asistirte.</h4>
-                </div>
-                <div id="correo"><h3>Correo Electrónico</h3></div>
-                <div id="horario"><h3>Horario</h3>
-                </div>
-                <div id="infohorario"><h4>¡Estamos a tu disposición! Nuestro equipo está disponible para atenderte en el siguiente horario:
-                    </h4></div>
-                <div id="infohorario2"><h4>Lunes a Sábado: 8:00 AM - 10:00 PM<h4></div>
-                            <div id="infocorreo"><h4>TechnoGame@gmail.com</h4>
-                            </div>
-                            <div id="infocorreo2"><h4>¿Quieres contactarnos por correo electrónico? Estamos listos para recibir tus mensajes y responder a tus consultas. 
-                                    ¡Escríbenos y uno de nuestros empleados se encargará de atender tu solicitud!</h4>
-                            </div>
-                            <div id="LogoTelefono">
-                                <img src="Imagenes/LogoTelefono.png" alt=""/>
-                            </div>
-                            <div id="LogoCorreo">
-                                <img src="Imagenes/LogoCorreo.png" alt=""/>
-                            </div>
-                            </header>
-                            <nav id="Contenedor-tiendas">
-                                <div id="titulotiendas"><h2>Nuestras tiendas</h2></div>
-                                <ul id="Listatiendas">
-                                    <li>
-                                        <div class="tienda">
-                                            <h3>Plaza Norte</h3>
-                                            <p>Centro Comercial Plaza Norte, Segundo Piso</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tienda">
-                                            <h3>Mega Plaza</h3>
-                                            <p>Centro Comercial Mega Plaza</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tienda">
-                                            <h3>Real Plaza</h3>
-                                            <p>Centro Comercial Real Plaza Primer piso</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tienda">
-                                            <h3>Mall Aventura Santa Anita</h3>
-                                            <p>Centro Comercial Mall Aventura Santa Anita</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tienda">
-                                            <h3>Plaza Sur</h3>
-                                            <p>Centro Comercial Plaza Sur, segundo Piso</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div class="tienda">
-                                            <h3>Jockey Plaza</h3>
-                                            <p>Centro Comercial Jockey Plaza</p>
-                                        </div>
-                                    </li>
-                                    <?php require_once './mainmapa.php' ?>;
-                                </ul>
-                            </nav>
-                            <main>
+                    
+                    <div class="item">
+                        <h2 class="title">Correo Electrónico</h2>
+                        <p class="info">¿Quieres contactarnos por correo electrónico? Estamos listos para recibir tus mensajes y responder a tus consultas. 
+                                ¡Escríbenos y uno de nuestros empleados se encargará de atender tu solicitud!</p>
 
-                            </main>
+                       <div class="cont_mail">
+                           <div><img src="Imagenes/LogoCorreo.png" alt=""/></div>
+                           <div class="mail"><h4>TechnoGame@gmail.com</h4></div>
+                       </div>
+                    </div>
+                    
+                </div>
+                
+           
+            </div>
+                             
+            <nav id="Contenedor-tiendas">
+                <div id="titulotiendas"><h2>Nuestras tiendas</h2></div>
+                <ul id="Listatiendas">
+                    <li>
+                        <div class="tienda">
+                            <h3>Plaza Norte</h3>
+                            <p>Centro Comercial Plaza Norte, Segundo Piso</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Mega Plaza</h3>
+                            <p>Centro Comercial Mega Plaza</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Real Plaza</h3>
+                            <p>Centro Comercial Real Plaza Primer piso</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Mall Aventura Santa Anita</h3>
+                            <p>Centro Comercial Mall Aventura Santa Anita</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Plaza Sur</h3>
+                            <p>Centro Comercial Plaza Sur, segundo Piso</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Jockey Plaza</h3>
+                            <p>Centro Comercial Jockey Plaza</p>
+                        </div>
+                    </li>
+                    <?php require_once './mainmapa.php'?>;
+                </ul>
+            </nav>
+   
+        </div>
+    </body>
+    <?php include('PieInicio.php'); ?>
+    
+</html>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>sw ABC</title>
+        <link href="CSS/EstiloContactenos_Ubiquenos.css" rel="stylesheet" type="text/css"/>
+    </head>
+        
+    <?php include('EncabezadoInicio.php'); ?>
+    <body>
+        <div id="contenedor-cont_ubic">
+ 
+            <div id="contacto"><center><h1>Contáctenos</h1></center></div>
+            <div class="container">
+                
+                <div class="redes">
+                    <h2 class="title">Redes sociales</h2>
+                    <p class="info_redes">¿Tienes alguna pregunta o inquietud? ¡Conéctate con nosotros en nuestras redes sociales y obtén respuestas rápidas y personalizadas! 
+                            Nuestro equipo está listo para atenderte y proporcionarte la información que necesitas. Únete a nuestra comunidad en las redes sociales y descubre una
+                            nueva forma de interactuar con nosotros. ¡Esperamos conocerte pronto!</p>
+                    
+                    <div class="container_imgs">
 
-                            <footer class="fijarCaja">
+                        <div class="itemImg"><a href="https://www.facebook.com"  class="enlace-imagen">
+                                <img src="Imagenes/Logo_de_Facebook.png" alt="Facebook"/>
+                            </a></div>
 
-                            </footer>
-                            </div>
-                            </body>
-                            <?php include('PieInicio.php'); ?>
+                            <div class="itemImg"><a href="https://www.youtube.com" class="enlace-imagen">
+                                <img src="Imagenes/LogoYoutube.png" alt="Youtube"/>
+                            </a></div>
+                            
+                            <div class="itemImg"><a href="https://www.instagram.com" class="enlace-imagen">
+                                <img src="Imagenes/LogoInstagram.jpg" alt="Instagram"/>
+                            </a></div>
+              
+                        
+                           <div class="itemImg"><a href="https://www.twitter.com" class="enlace-imagen">
+                                <img src="Imagenes/LogoTwitter.jpg" alt="Twitter"/>
+                            </a></div>
+                    </div>           
+                </div> <!-- Contenedor para apartado redes-->
+                
+                <div class="container_contacto">
+                    
+                    <div class="item">
+                       <h2 class="title">Teléfono</h2>
+                       <p class="info">¿Necesitas ayuda? Llámanos. Nuestro equipo estará encantado de atenderte en horario laboral. 
+                               Si nos contactas fuera de ese horario, nuestro bot te proporcionará información básica hasta que estemos disponibles para asistirte.</p>
+                       <div class="cont_number">
+                           <div><img src="Imagenes/LogoTelefono.png" alt=""/></div>
+                           <div class="number"><h4>(+51)964593567</h4></div>
+                       </div>
+                    </div>
 
-                            </html>
+                    <div class="item">
+                        <h2 class="title">Horario</h2>
+                        <p class="info">¡Estamos a tu disposición! Nuestro equipo está disponible para atenderte en el siguiente horario:</p>
+                        <p class="info_h">Lunes a Sábado: 8:00 AM - 10:00 PM<p>
+                    </div>
+                    
+                    <div class="item">
+                        <h2 class="title">Correo Electrónico</h2>
+                        <p class="info">¿Quieres contactarnos por correo electrónico? Estamos listos para recibir tus mensajes y responder a tus consultas. 
+                                ¡Escríbenos y uno de nuestros empleados se encargará de atender tu solicitud!</p>
+
+                       <div class="cont_mail">
+                           <div><img src="Imagenes/LogoCorreo.png" alt=""/></div>
+                           <div class="mail"><h4>TechnoGame@gmail.com</h4></div>
+                       </div>
+                    </div>
+                    
+                </div>
+                
+           
+            </div>
+                             
+            <nav id="Contenedor-tiendas">
+                <div id="titulotiendas"><h2>Nuestras tiendas</h2></div>
+                <ul id="Listatiendas">
+                    <li>
+                        <div class="tienda">
+                            <h3>Plaza Norte</h3>
+                            <p>Centro Comercial Plaza Norte, Segundo Piso</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Mega Plaza</h3>
+                            <p>Centro Comercial Mega Plaza</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Real Plaza</h3>
+                            <p>Centro Comercial Real Plaza Primer piso</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Mall Aventura Santa Anita</h3>
+                            <p>Centro Comercial Mall Aventura Santa Anita</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Plaza Sur</h3>
+                            <p>Centro Comercial Plaza Sur, segundo Piso</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="tienda">
+                            <h3>Jockey Plaza</h3>
+                            <p>Centro Comercial Jockey Plaza</p>
+                        </div>
+                    </li>
+                    <?php require_once './mainmapa.php'?>;
+                </ul>
+            </nav>
+   
+        </div>
+    </body>
+    <?php include('PieInicio.php'); ?>
+    
+</html>
