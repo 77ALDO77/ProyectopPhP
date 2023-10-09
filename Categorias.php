8ae335d6bc10dbccdfa5872e830181894a08a1b7
Raul_LA <rleonalvarez090@gmail.com>
2023-10-09 03:37:23 -0500

    Barra Categorias

---
+++ Categorias.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="CSS/Categorias.css" rel="stylesheet" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;500&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body >
+    <header2 class="bodyCate">
+     <nav class="navCateg">
+        <ul class="cont-ul">
+            <li class="el">Item 1</li>
+            <li class="el">Item 2</li>
+            <li class="develop">Gaming
+                <ul class="ul-second">
+                    <li class="el">Item 3.1</li>
+                    <li class="front">
+                        <a href="CatTeclados.php">Teclados</a>
+                        <ul class="ul-third">
+                            <li class="el">Razer</li>
+                            <li class="el">Item 3.2.2</li>
+                            <li class="el">Item 3.2.3</li>
+                        </ul>
+                    </li>
+                    <li class="el">3.3</li>
+                </ul>
+            </li>
+            <li class="el">Item 4</li>
+            <li class="el">Item 5</li>
+        </ul>
+    </nav>
+    </header2>
+</body>
+
+</html>
