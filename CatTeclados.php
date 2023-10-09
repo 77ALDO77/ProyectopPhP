225a85fa1d4d8a2a7e94850166ff387fd2e9dd4d
Raul_LA <rleonalvarez090@gmail.com>
2023-10-09 03:37:53 -0500

    Categoria Teclados

---
+++ CatTeclados.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <?php include('EncabezadoInicio.php'); ?>
+        
+        
+        Pagina de centro
+        
+        
+        
+        <?php include('PieInicio.php'); ?>
+        
+    </body>
+</html>
