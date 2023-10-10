3ec96662fbe0dfad303d308a4ad9f3c53a202f53
Raul_LA <rleonalvarez090@gmail.com>
2023-10-10 00:26:26 -0500

    mod FrmSugerencias (+)notificacion

--- FrmSugerencias.php
+++ FrmSugerencias.php
@@ -4,20 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Sugerencias</title>
+    <!<!-- Estilos CSS -->
     <link href="CSS/EncabezadoInicio.css" rel="stylesheet" type="text/css"/>
     <link href="CSS/PieInicio.css?uuid=<?php echo uniqid();?>" rel="stylesheet" type="text/css"/>
     <link href="CSS/StyleFrmSugerencias.css" rel="stylesheet" type="text/css"/>
-    <script src="https://kit.fontawesome.com/c971f825af.js" crossorigin="anonymous"></script>
     
+    <!<!-- Para las ventanas emergentes -->
+    <script src="https://kit.fontawesome.com/c971f825af.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.7.31/sweetalert2.min.js" integrity="sha512-dbgWBkIauIf3iy96dqgzBD9ysKHp7mAuym+V7AqaNIuICxDBVm6nzvl1Yi+rdfnh25SdmYDw2JbFk/FOXf6Ycg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.7.31/sweetalert2.min.css" integrity="sha512-IScV5kvJo+TIPbxENerxZcEpu9VrLUGh1qYWv6Z9aylhxWE4k4Fch3CHl0IYYmN+jrnWQBPlpoTVoWfSMakoKA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body>
+    <script type="text/javascript">
+        function validar(){
+            var nombre= document.forms[0]["name"].value;
+        
+            if(nombre=="" ){
+                Swal.fire({
+                  icon: 'error',
+                  text: 'Datos invalidos',
+                })
+            }else{
+                Swal.fire({
+                  icon: 'success',
+                  text: 'Informacion enviada',
+                })
+
+            }
+            return false;
+
+        }
+        
+    
+    </script>
+    
     <?php include('EncabezadoInicio.php'); ?>   
     
     <!-- CONTENIDO Formulario Reclamaciones-->
     <main> 
 
-        <form class="form" action="ProcesaForms.php" method="POST">
+        <form class="form" method="post" action="ProcesaForms.php" onsubmit="return validar()">
             <div class="title-block">
                 <h1 class="form_title">Formulario de Sugerencias</h1>
             </div>
@@ -25,17 +53,17 @@
             <p class="form_paragraph">Agradecemos que puedas dejarnos tus sugerencias</p><br><br>
 
             <div class="form_container">
-                <input type="text" id="nombre" class="input" placeholder=" " required="">   
+                <input type="text" name="name" id="nombre" class="input" placeholder=" ">   
                 <label for="nombre" class="label"><i class="fa-solid fa-user"></i> Nombres y Apellidos:</label>
             </div>
 
             <div class="form_container">
-                <input type="email" id="email" class="input" placeholder=" " required="">   
+                <input type="email" name="email" id="email" class="input" placeholder=" " required="">   
                 <label for="email" class="label"><i class="fa-solid fa-envelope"></i> Correo:</label>
             </div>
 
             <div class="form_container">
-                <input type="text" id="numero" class="input" placeholder=" " required="">   
+                <input type="text" id="numero" name="number" class="input" placeholder=" " required="">   
                 <label for="numero" class="label"><i class="fa-solid fa-phone"></i> Celular:</label>
             </div>
 
