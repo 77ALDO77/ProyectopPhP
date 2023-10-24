6d576ec52a7e1fdf24d1012b783b08de7f9a8dc8
Raul_LA <rleonalvarez090@gmail.com>
2023-10-24 15:59:55 -0500

    encabezado repetido

--- CatTeclados.php
+++ CatTeclados.php
@@ -8,7 +8,6 @@
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <script src="https://kit.fontawesome.com/c971f825af.js" crossorigin="anonymous"></script>
-   <?php include('EncabezadoInicio.php'); ?>
     </head>
     <body>
             
