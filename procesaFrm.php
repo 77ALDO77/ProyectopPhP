12c162382c04869aa1b4177ee663f3b48a16ccfb
Raul_LA <rleonalvarez090@gmail.com>
2023-11-07 16:11:15 -0500

    (+) metodo insertar el reclamo en la bd

--- procesaFrm.php
+++ procesaFrm.php
@@ -1,18 +1,27 @@
 <?php
+    require_once './ConectaDB.php';
+    $cn = getConexion();
+?> 
+
+<?php
 if(isset($_REQUEST["accion"])){
     $accion=$_REQUEST["accion"];
 }
 if($accion=="add"){
     $cod=$_POST["nombre"];
     $nom=$_POST["domicilio"];
-    $pre=$_POST["dni"];
-    $pre=$_POST["emil"];
-    $pre=$_POST["numero"];
+    $dni=$_POST["dni"];
+    $email=$_POST["email"];
+    $numero=$_POST["numero"];
     
-    $pre=$_POST["monto"];
-    $pre=$_POST["descripcion"];
-    $pre=$_POST["pedido"];
-  
+    $tipoBien="Producto";
+    $monto=$_POST["monto"];
+    $descripcion=$_POST["descripcion"];
+    
+    $tipoReclamo="Queja";
+    $detalle=$_POST["detalle"];
+    $pedido=$_POST["pedido"];
+    
     $foto=$_FILES["foto"];
     $nombreArchivo = $foto['name'];
     $tipoMIME = $foto['type'];
@@ -24,7 +33,7 @@ if($accion=="add"){
 
         if ($tipoMIME === 'image/jpeg' || $tipoMIME === 'image/png') {
                 
-            $nombreArchivoDestino = 'images/'; 
+            $nombreArchivoDestino = 'imgsReclamos/'; 
 
             if (move_uploaded_file($nombreArchivoTemporal, $nombreArchivoDestino.$nombreArchivo)) {
                 echo "El archivo se ha cargado y movido correctamente.";
@@ -38,6 +47,30 @@ if($accion=="add"){
     } else {
         echo "Error al subir";
     }
-}
+    
 echo $accion;
+
+    $sql = "INSERT INTO Reclamaciones (NomApe, Domicilio, Dni,Email,Numero,"
+            . "tipoBien,Monto,Descripcion,TipoReclamo,Detalle,Pedido) "
+            . "VALUES (?, ?, ? ,? ,? ,? ,? ,?, ?, ?, ?)";
+    $sentencia = $cn->prepare($sql);
+    $tipoDatos = "sssssssssss"; 
+    if ($sentencia) {
+        // Vincular los parámetros y sus tipos de datos
+        $sentencia->bind_param($tipoDatos,$cod, $nom, $dni,$email,$numero,
+                $tipoBien,$monto,$descripcion,
+                $tipoReclamo,$detalle,$pedido);
+
+        // Ejecutar la sentencia
+        if ($sentencia->execute()) {
+            echo "Inserción exitosa";
+        } else {
+            echo "Error en la inserción: " . $sentencia->error;
+        }
+
+    } else {
+        echo "Error en la preparación de la sentencia: " . $conexion->error;
+    }
+    
+}
 
