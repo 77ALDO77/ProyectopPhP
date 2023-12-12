0889727129efecc29d3e2a2c81876b65a7a4d01b
Raul_LA <rleonalvarez090@gmail.com>
2023-12-11 21:36:34 -0500

    conexion db como estaba antes

--- ConectaDB.php
+++ ConectaDB.php
@@ -2,7 +2,7 @@
 function getConexion(){
     $hostname = "localhost";
     $username = "root";
-    $password = "123123"; 
+    $password = ""; 
     $database ="technogame"; 
     $port="3306";
     $cn = mysqli_connect($hostname,$username,$password,$database,$port)or die("Error al conectar");
